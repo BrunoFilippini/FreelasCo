a1ff51344caf5993b33bcbb0582ca2092dd41d0d
Bruno Filippini <brunofilippini94@gmail.com>
2022-02-23 19:24:02 -0300

    change colors

--- src/components/CardProjects/CardProjects.js
+++ src/components/CardProjects/CardProjects.js
@@ -15,62 +15,6 @@ export function CardProject(props) {
   }
 
   return (
-    /*     <div className={styles.main}>
-      <div
-        className={`${styles.profile} ${
-          expanded ? styles["profile--expanded"] : styles["profile--unexpanded"]
-        }`}
-        onClick={() => {
-          setExpanded((prevState) => !prevState);
-        }}
-      >
-        <div className={styles.profileBanner}></div>
-        <div>
-          <div className={styles.profilePic}>
-            <img
-              className={styles.imgPerson}
-              src={props.img}
-              alt={`Foto de ${props.nameProject}`}
-            />
-          </div>
-          <div className={styles.profileInfo}>
-            <span className={styles.profileName}>{props.nameProject}</span>
-            <span className={styles.profileInfoUsername}>{props.area}</span>
-            <span className={styles.profileInfoUsernameB}>
-              <b>{props.description}</b>
-            </span>
-          </div>
-        </div>
-        <div className={styles.profileData}>
-          <span className={styles.profileDataFollowing}>
-            <p className={styles.profileDataStartDate}>
-              <b>Data de início: </b>
-              {props.startDate}
-            </p>
-            <p className={styles.profileDataBudget}>
-              <b>Budget: </b> ${props.budget}
-            </p>
-            <p className={styles.profileDataStartDetails}>
-              <b>Detalhes: </b>
-              {props.details}
-            </p>
-            <p className={styles.profileDataContact}>
-              <b>Contato: </b>
-              {props.contact}
-            </p>
-            <Link className={styles.textLink} to={`/edit-Project/${props.id}`}>
-              <button type="button" className={styles.btn}>
-                Editar Perfil
-              </button>
-            </Link>
-            <button type="button" onClick={handleDelete} className={styles.btn}>
-              Deletar Perfil
-            </button>
-          </span>
-        </div>
-      </div>
-    </div> */
-
     <div className={styles.flipCardContainer}>
       <div className={styles.flipCard}>
         <div className={styles.cardFront}>
